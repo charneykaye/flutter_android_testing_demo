842316b5862b6b9a951c2cc01b28b6651b7eacd6
Nick Charney Kaye <charneykaye@gmail.com>
2024-11-26 14:23:13 -0800

    Add Android test

---
+++ android/app/src/androidTest/java/com/example/flutter_android_testing_demo/ScreengrabTest.java
@@ -0,0 +1,48 @@
+package com.example.flutter_android_testing_demo;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.uiautomator.UiDevice;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import tools.fastlane.screengrab.Screengrab;
+import tools.fastlane.screengrab.UiAutomatorScreenshotStrategy;
+import tools.fastlane.screengrab.cleanstatusbar.BluetoothState;
+import tools.fastlane.screengrab.cleanstatusbar.CleanStatusBar;
+import tools.fastlane.screengrab.cleanstatusbar.MobileDataType;
+import tools.fastlane.screengrab.locale.LocaleTestRule;
+
+public class ScreengrabTest {
+    @ClassRule
+    public static final LocaleTestRule localeTestRule = new LocaleTestRule();
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @BeforeClass
+    public static void beforeAll() {
+        Screengrab.setDefaultScreenshotStrategy(new UiAutomatorScreenshotStrategy());
+
+        new CleanStatusBar()
+                .setMobileNetworkDataType(MobileDataType.LTE)
+                .setBluetoothState(BluetoothState.DISCONNECTED)
+                .enable();
+    }
+
+    @AfterClass
+    public static void afterAll() {
+        CleanStatusBar.disable();
+    }
+
+    @Test
+    public void testTakeScreenshots() throws Exception {
+        // Take screenshot
+        Screengrab.screenshot("0_Example");
+    }
+}
+
